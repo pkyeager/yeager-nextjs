d2cfe893e69ef835cad0c5772442261f9e82c0b3
Phongsakon Konrad <124145979+pkyeager@users.noreply.github.com>
2024-02-07 07:44:02 +0100

    Update footer links in footer component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -12,33 +12,33 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 items-center w-full mt-36 text-center">
       <div className="container mx-auto flex justify-center space-x-4">
-        <a href="/legal" className="mb-2 text-sm">
-          Legal Notice
-        </a>
+        <Link legacyBehavior href="legal">
+          <a className="mb-2 text-sm">Legal Notice</a>
+        </Link>
 
-        <a href="/terms" className="mb-2 text-sm" onClick={handleLinkClick}>
-          Terms of Service
-        </a>
-        <a href="/privacy" className="mb-2 text-sm" onClick={handleLinkClick}>
-          Privacy
-        </a>
-        <a href="/contact" className="mb-2 text-sm" onClick={handleLinkClick}>
-          Contact Us
-        </a>
-        <a
+        <Link legacyBehavior href="/terms" onClick={handleLinkClick}>
+          <a className="mb-2 text-sm">Terms of Service</a>
+        </Link>
+        <Link legacyBehavior href="/privacy" onClick={handleLinkClick}>
+          <a className="mb-2 text-sm">Privacy</a>
+        </Link>
+        <Link legacyBehavior href="/contact" onClick={handleLinkClick}>
+          <a className="mb-2 text-sm">Contact Us</a>
+        </Link>
+        <Link
+          legacyBehavior
           href="https://twitter.com/yourusername"
-          className="mb-2 text-sm"
           onClick={handleLinkClick}
         >
-          X
-        </a>
-        <a
+          <a className="mb-2 text-sm">X</a>
+        </Link>
+        <Link
+          legacyBehavior
           href="https://instagram.com/yourusername"
-          className="mb-2 text-sm"
           onClick={handleLinkClick}
         >
-          Instagram
-        </a>
+          <a className="mb-2 text-sm">Instagram</a>
+        </Link>
       </div>
       <div className="mt-4">
         {/* <p className="text-xs">© {new Date().getFullYear()} Yeager GmbH</p> */}
